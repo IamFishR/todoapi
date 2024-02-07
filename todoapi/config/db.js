788702fc96aad3c17c8e57be54726c8ffbb0a83e
Rakesh Jadhav <thisisganesh353@gmail.com>
2024-02-08 02:55:31 +0530

    Add database schema documentation

--- todoapi/config/db.js
+++ todoapi/config/db.js
@@ -20,3 +20,104 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+// - collections/
+//     - users/
+//         - _id
+//         - username String
+//         - email String
+//         - password String
+//         - roleids [] (array of role ids)
+//         - createdAt Date
+//         - updatedAt Date
+//         - additionalInfo {} (object of additional information)
+//             - status
+//             - bio
+//             - website
+//             - github
+//             - outlook
+//             - google
+//             - calendly
+//             - microsoft_teams
+//     - role ('superadmin', 'admin', 'user', 'support', 'guest')
+//         - _id
+//         - rolename 
+//         - createdAt
+//         - updatedAt
+//     - controllers/
+//         - _id
+//         - controllername
+//         - createdAt
+//         - updatedAt
+//     - actions/
+//         - _id
+//         - actionname
+//         - controllerId
+//         - createdAt
+//         - updatedAt
+//     - rolepermissions/
+//         - _id
+//         - roleId
+//         - controllerId
+//         - actionId
+//         - createdAt
+//         - updatedAt
+//     - todos/
+//         - _id
+//         - title
+//         - description
+//         - dueDate
+//         - priority
+//         - status (want to do, doing it, done, have done some part, doing it later, do not want to do, do or die, etc)
+//         - projectId []
+//         - tags [] (tagnames as string)
+//         - attachments []
+//         - userId
+//         - createdAt
+//         - updatedAt
+//     - subtasks/
+//         - _id
+//         - title
+//         - description
+//         - dueDate
+//         - priority
+//         - status (want to do, doing it, done, have done some part, doing it later, do not want to do, do or die, etc)
+//         - createdAt
+//         - updatedAt
+//         - todoId
+//     - comments/
+//         - _id
+//         - refId (reference id) - can be todoId, subtaskId, projectId, etc
+//         - comments [] (array of comments)
+//             - createdAt
+//             - updatedAt
+//             - comment
+//             - userInfo {}
+//                 - userId
+//                 - username
+//         - otherInfo {}
+//             - ispinned
+//     - attachments/
+//         - _id
+//         - refId (reference id) - can be todoId, subtaskId, projectId, etc
+//         - name
+//         - contentType (image, video, audio, document, etc)
+//         - url
+//         - createdAt
+//     - categories/
+//         - _id
+//         - name
+//         - color
+//     - projects/
+//         - _id
+//         - name
+//         - description
+//         - color
+//         - refId (reference id) - todoId
+//         - tagIds []
+//         - categoryIds []
+//         - createdAt
+//         - updatedAt
+//         - dueDate
+//         - status (want to do, doing it, done, have done some part, doing it later, do not want to do, do or die, etc)
+//         - priority (high, medium, low)
