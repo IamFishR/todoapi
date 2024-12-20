54bde134caa5120e6f0799152185b44265456c7c
Rakesh Jadhav <thisisganesh353@gmail.com>
2024-12-20 14:51:36 +0530

    refactor: Simplify shop route paths by removing redundant '/shop' prefix

--- routes/shop.js
+++ routes/shop.js
@@ -3,37 +3,37 @@ var router = express.Router();
 const shopController = require('../controllers/shopController');
 
 // Shop routes
-router.post('/shop', shopController.addShop);
-router.put('/shop/:id', shopController.updateShop);
-router.delete('/shop/:id', shopController.deleteShop);
+router.post('/', shopController.addShop);
+router.put('/:id', shopController.updateShop);
+router.delete('/:id', shopController.deleteShop);
 
 // Category routes
-router.post('/shop/:shopId/category', shopController.addCategory);
-router.put('/shop/:shopId/category/:id', shopController.updateCategory);
-router.delete('/shop/:shopId/category/:id', shopController.deleteCategory);
+router.post('/:shopId/category', shopController.addCategory);
+router.put('/:shopId/category/:id', shopController.updateCategory);
+router.delete('/:shopId/category/:id', shopController.deleteCategory);
 
 // Product routes
-router.post('/shop/:shopId/category/:categoryId/product', shopController.addProduct);
-router.put('/shop/:shopId/category/:categoryId/product/:id', shopController.updateProduct);
-router.delete('/shop/:shopId/category/:categoryId/product/:id', shopController.deleteProduct);
+router.post('/:shopId/category/:categoryId/product', shopController.addProduct);
+router.put('/:shopId/category/:categoryId/product/:id', shopController.updateProduct);
+router.delete('/:shopId/category/:categoryId/product/:id', shopController.deleteProduct);
 
 // Product quantity routes
-router.post('/shop/:shopId/category/:categoryId/product/:productId/quantity', shopController.addProductQuantityHistory);
-router.get('/shop/:shopId/category/:categoryId/product/:productId/quantity', shopController.getProductQuantityHistory);
+router.post('/:shopId/category/:categoryId/product/:productId/quantity', shopController.addProductQuantityHistory);
+router.get('/:shopId/category/:categoryId/product/:productId/quantity', shopController.getProductQuantityHistory);
 
 // Order routes
-router.post('/shop/:shopId/order', shopController.addOrder);
-router.put('/shop/:shopId/order/:id', shopController.updateOrder);
-router.delete('/shop/:shopId/order/:id', shopController.deleteOrder);
+router.post('/:shopId/order', shopController.addOrder);
+router.put('/:shopId/order/:id', shopController.updateOrder);
+router.delete('/:shopId/order/:id', shopController.deleteOrder);
 
 // Promotion routes
-router.post('/shop/:shopId/promotion', shopController.addPromotion);
-router.put('/shop/:shopId/promotion/:id', shopController.updatePromotion);
-router.delete('/shop/:shopId/promotion/:id', shopController.deletePromotion);
+router.post('/:shopId/promotion', shopController.addPromotion);
+router.put('/:shopId/promotion/:id', shopController.updatePromotion);
+router.delete('/:shopId/promotion/:id', shopController.deletePromotion);
 
 // Review routes
-router.post('/shop/:shopId/product/:productId/review', shopController.addReview);
-router.put('/shop/:shopId/product/:productId/review/:id', shopController.updateReview);
-router.delete('/shop/:shopId/product/:productId/review/:id', shopController.deleteReview);
+router.post('/:shopId/product/:productId/review', shopController.addReview);
+router.put('/:shopId/product/:productId/review/:id', shopController.updateReview);
+router.delete('/:shopId/product/:productId/review/:id', shopController.deleteReview);
 
 module.exports = router;
